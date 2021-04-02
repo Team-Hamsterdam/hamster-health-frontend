52713a36798ee413a690767a467822d487a34e1e
Matthew Lau <matthew.lau78@gmail.com>
2021-04-03 00:35:28 +1100

    navbar fix

--- frontend/src/components/Navbar1.js
+++ frontend/src/components/Navbar1.js
@@ -1,18 +1,22 @@
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Form from "react-bootstrap/Form";
-import FormControl from "react-bootstrap/FormControl";
-import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
+import logo from "../logo.png";
 const Navbar1 = () => {
     return (
         <Navbar variant="dark" expand="lg">
-            <Navbar.Brand href="#home">Hamster Health</Navbar.Brand>
+            <Image style={{ width: 60, height: 60 }} src={logo} />
+            <Navbar.Brand href="/">Hamster Health</Navbar.Brand>
+
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
-                    <Button className="mx-1">Sign Up</Button>
-                    <Button className="mx-1">Sign In</Button>
+                    <a className="btn btn-primary mx-1 my-1" href="/register">
+                        Sign Up
+                    </a>
+                    <a className="btn btn-primary mx-1 my-1" href="/login">
+                        Sign In
+                    </a>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
