9a65eb3a189f54b5b0f3cc260a41c8daffd3c0db
Matthew Lau <matthew.lau78@gmail.com>
2021-04-02 23:43:50 +1100

    routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
-import Landing from './Landing'
-import Navbar1 from './components/Navbar1'
+import "./App.css";
+import Landing from "./Landing";
+import Navbar1 from "./components/Navbar1";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <Navbar1 />
-    </>
-  );
+    return (
+        <Router>
+            <Navbar1 />
+            <Route exact path="/" component={Landing} />
+        </Router>
+    );
 }
 
 export default App;
