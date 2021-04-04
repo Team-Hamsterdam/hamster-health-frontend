92a78ccd5860662106421929a58ef08ed8bf6274
Matthew Lau <matthew.lau78@gmail.com>
2021-04-04 22:20:26 +1000

    landing page text

--- frontend/src/components/sections/FeaturesTiles.js
+++ frontend/src/components/sections/FeaturesTiles.js
@@ -5,230 +5,191 @@ import SectionHeader from "./partials/SectionHeader";
 import Image from "../elements/Image";
 
 const propTypes = {
-    ...SectionTilesProps.types,
+  ...SectionTilesProps.types,
 };
 
 const defaultProps = {
-    ...SectionTilesProps.defaults,
+  ...SectionTilesProps.defaults,
 };
 const FeaturesTiles = ({
-    className,
-    topOuterDivider,
-    bottomOuterDivider,
-    topDivider,
-    bottomDivider,
-    hasBgColor,
-    invertColor,
-    pushLeft,
-    ...props
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  pushLeft,
+  ...props
 }) => {
-    const outerClasses = classNames(
-        "features-tiles section",
-        topOuterDivider && "has-top-divider",
-        bottomOuterDivider && "has-bottom-divider",
-        hasBgColor && "has-bg-color",
-        invertColor && "invert-color",
-        className
-    );
+  const outerClasses = classNames(
+    "features-tiles section",
+    topOuterDivider && "has-top-divider",
+    bottomOuterDivider && "has-bottom-divider",
+    hasBgColor && "has-bg-color",
+    invertColor && "invert-color",
+    className
+  );
 
-    const innerClasses = classNames(
-        "features-tiles-inner section-inner pt-0",
-        topDivider && "has-top-divider",
-        bottomDivider && "has-bottom-divider"
-    );
+  const innerClasses = classNames(
+    "features-tiles-inner section-inner pt-0",
+    topDivider && "has-top-divider",
+    bottomDivider && "has-bottom-divider"
+  );
 
-    const tilesClasses = classNames(
-        "tiles-wrap center-content",
-        pushLeft && "push-left"
-    );
+  const tilesClasses = classNames("tiles-wrap center-content", pushLeft && "push-left");
 
-    const sectionHeader = {
-        title: "Improve Your Lifestyle",
-        paragraph:
-            "Quarantine is the perfect time to chase your goals. Eat something healthy, learn something new or maybe make a new friend.",
-    };
+  const sectionHeader = {
+    title: "Improve Your Lifestyle",
+    paragraph:
+      "Quarantine has given people a lot of extra time but has also made it harder for some to stay on track with their usual commitments.",
+  };
 
-    return (
-        <section {...props} className={outerClasses}>
-            <div className="container">
-                <div className={innerClasses}>
-                    <SectionHeader
-                        data={sectionHeader}
-                        className="center-content"
+  return (
+    <section {...props} className={outerClasses}>
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={tilesClasses}>
+            <div className="tiles-item reveal-from-bottom">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require("./../../assets/images/feature-tile-icon-01.svg")}
+                      alt="Features tile icon 01"
+                      width={64}
+                      height={64}
                     />
-                    <div className={tilesClasses}>
-                        <div className="tiles-item reveal-from-bottom">
-                            <div className="tiles-item-inner">
-                                <div className="features-tiles-item-header">
-                                    <div className="features-tiles-item-image mb-16">
-                                        <Image
-                                            src={require("./../../assets/images/feature-tile-icon-01.svg")}
-                                            alt="Features tile icon 01"
-                                            width={64}
-                                            height={64}
-                                        />
-                                    </div>
-                                </div>
-                                <div className="features-tiles-item-content">
-                                    <h4 className="mt-0 mb-8">
-                                        Robust Workflow
-                                    </h4>
-                                    <p className="m-0 text-sm">
-                                        Duis aute irure dolor in reprehenderit
-                                        in voluptate velit esse cillum dolore eu
-                                        fugiat nulla pariatur. Excepteur sint
-                                        occaecat cupidatat.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div
-                            className="tiles-item reveal-from-bottom"
-                            data-reveal-delay="200"
-                        >
-                            <div className="tiles-item-inner">
-                                <div className="features-tiles-item-header">
-                                    <div className="features-tiles-item-image mb-16">
-                                        <Image
-                                            src={require("./../../assets/images/feature-tile-icon-02.svg")}
-                                            alt="Features tile icon 02"
-                                            width={64}
-                                            height={64}
-                                        />
-                                    </div>
-                                </div>
-                                <div className="features-tiles-item-content">
-                                    <h4 className="mt-0 mb-8">
-                                        Robust Workflow
-                                    </h4>
-                                    <p className="m-0 text-sm">
-                                        Duis aute irure dolor in reprehenderit
-                                        in voluptate velit esse cillum dolore eu
-                                        fugiat nulla pariatur. Excepteur sint
-                                        occaecat cupidatat.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div
-                            className="tiles-item reveal-from-bottom"
-                            data-reveal-delay="400"
-                        >
-                            <div className="tiles-item-inner">
-                                <div className="features-tiles-item-header">
-                                    <div className="features-tiles-item-image mb-16">
-                                        <Image
-                                            src={require("./../../assets/images/feature-tile-icon-03.svg")}
-                                            alt="Features tile icon 03"
-                                            width={64}
-                                            height={64}
-                                        />
-                                    </div>
-                                </div>
-                                <div className="features-tiles-item-content">
-                                    <h4 className="mt-0 mb-8">
-                                        Robust Workflow
-                                    </h4>
-                                    <p className="m-0 text-sm">
-                                        Duis aute irure dolor in reprehenderit
-                                        in voluptate velit esse cillum dolore eu
-                                        fugiat nulla pariatur. Excepteur sint
-                                        occaecat cupidatat.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="tiles-item reveal-from-bottom">
-                            <div className="tiles-item-inner">
-                                <div className="features-tiles-item-header">
-                                    <div className="features-tiles-item-image mb-16">
-                                        <Image
-                                            src={require("./../../assets/images/feature-tile-icon-04.svg")}
-                                            alt="Features tile icon 04"
-                                            width={64}
-                                            height={64}
-                                        />
-                                    </div>
-                                </div>
-                                <div className="features-tiles-item-content">
-                                    <h4 className="mt-0 mb-8">
-                                        Robust Workflow
-                                    </h4>
-                                    <p className="m-0 text-sm">
-                                        Duis aute irure dolor in reprehenderit
-                                        in voluptate velit esse cillum dolore eu
-                                        fugiat nulla pariatur. Excepteur sint
-                                        occaecat cupidatat.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div
-                            className="tiles-item reveal-from-bottom"
-                            data-reveal-delay="200"
-                        >
-                            <div className="tiles-item-inner">
-                                <div className="features-tiles-item-header">
-                                    <div className="features-tiles-item-image mb-16">
-                                        <Image
-                                            src={require("./../../assets/images/feature-tile-icon-05.svg")}
-                                            alt="Features tile icon 05"
-                                            width={64}
-                                            height={64}
-                                        />
-                                    </div>
-                                </div>
-                                <div className="features-tiles-item-content">
-                                    <h4 className="mt-0 mb-8">
-                                        Robust Workflow
-                                    </h4>
-                                    <p className="m-0 text-sm">
-                                        Duis aute irure dolor in reprehenderit
-                                        in voluptate velit esse cillum dolore eu
-                                        fugiat nulla pariatur. Excepteur sint
-                                        occaecat cupidatat.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div
-                            className="tiles-item reveal-from-bottom"
-                            data-reveal-delay="400"
-                        >
-                            <div className="tiles-item-inner">
-                                <div className="features-tiles-item-header">
-                                    <div className="features-tiles-item-image mb-16">
-                                        <Image
-                                            src={require("./../../assets/images/feature-tile-icon-06.svg")}
-                                            alt="Features tile icon 06"
-                                            width={64}
-                                            height={64}
-                                        />
-                                    </div>
-                                </div>
-                                <div className="features-tiles-item-content">
-                                    <h4 className="mt-0 mb-8">
-                                        Robust Workflow
-                                    </h4>
-                                    <p className="m-0 text-sm">
-                                        Duis aute irure dolor in reprehenderit
-                                        in voluptate velit esse cillum dolore eu
-                                        fugiat nulla pariatur. Excepteur sint
-                                        occaecat cupidatat.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                  </div>
                 </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">Tasks</h4>
+                  <p className="m-0 text-sm">
+                    Hamster Health helps you create a daily list of goals for you to achieve. It
+                    allows you to create any task you want so you can choose to hit the books or hit
+                    the gym.
+                  </p>
+                </div>
+              </div>
             </div>
-        </section>
-    );
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require("./../../assets/images/feature-tile-icon-02.svg")}
+                      alt="Features tile icon 02"
+                      width={64}
+                      height={64}
+                    />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">Progress Tracker </h4>
+                  <p className="m-0 text-sm">
+                    Hamster Health holds on to your task progress over the last fortnight. You can
+                    check how many of your tasks you completed for a certain day to see if you're
+                    getting closer to your goals or if you still need to improve a bit.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require("./../../assets/images/feature-tile-icon-03.svg")}
+                      alt="Features tile icon 03"
+                      width={64}
+                      height={64}
+                    />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">Keep up a streak</h4>
+                  <p className="m-0 text-sm">
+                    As you complete tasks you gain experience to level up your profile. The more
+                    tasks you complete the higher your level. Hamster Health even includes a
+                    leaderboard of the users with the 50 highest levels!
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            {/*<div className="tiles-item reveal-from-bottom">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require("./../../assets/images/feature-tile-icon-04.svg")}
+                      alt="Features tile icon 04"
+                      width={64}
+                      height={64}
+                    />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">Robust Workflow</h4>
+                  <p className="m-0 text-sm">
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require("./../../assets/images/feature-tile-icon-05.svg")}
+                      alt="Features tile icon 05"
+                      width={64}
+                      height={64}
+                    />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">Robust Workflow</h4>
+                  <p className="m-0 text-sm">
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require("./../../assets/images/feature-tile-icon-06.svg")}
+                      alt="Features tile icon 06"
+                      width={64}
+                      height={64}
+                    />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">Robust Workflow</h4>
+                  <p className="m-0 text-sm">
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
+                  </p>
+                </div>
+              </div>
+            </div> */}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 FeaturesTiles.propTypes = propTypes;
--- frontend/src/components/sections/Hero.js
+++ frontend/src/components/sections/Hero.js
@@ -8,121 +8,104 @@ import Modal from "../elements/Modal";
 import { Link } from "react-router-dom";
 
 const propTypes = {
-    ...SectionProps.types,
+  ...SectionProps.types,
 };
 
 const defaultProps = {
-    ...SectionProps.defaults,
+  ...SectionProps.defaults,
 };
 
 const Hero = ({
-    className,
-    topOuterDivider,
-    bottomOuterDivider,
-    topDivider,
-    bottomDivider,
-    hasBgColor,
-    invertColor,
-    ...props
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  ...props
 }) => {
-    const [videoModalActive, setVideomodalactive] = useState(false);
+  const [videoModalActive, setVideomodalactive] = useState(false);
 
-    const openModal = (e) => {
-        e.preventDefault();
-        setVideomodalactive(true);
-    };
+  const openModal = (e) => {
+    e.preventDefault();
+    setVideomodalactive(true);
+  };
 
-    const closeModal = (e) => {
-        e.preventDefault();
-        setVideomodalactive(false);
-    };
+  const closeModal = (e) => {
+    e.preventDefault();
+    setVideomodalactive(false);
+  };
 
-    const outerClasses = classNames(
-        "hero section center-content",
-        topOuterDivider && "has-top-divider",
-        bottomOuterDivider && "has-bottom-divider",
-        hasBgColor && "has-bg-color",
-        invertColor && "invert-color",
-        className
-    );
+  const outerClasses = classNames(
+    "hero section center-content",
+    topOuterDivider && "has-top-divider",
+    bottomOuterDivider && "has-bottom-divider",
+    hasBgColor && "has-bg-color",
+    invertColor && "invert-color",
+    className
+  );
 
-    const innerClasses = classNames(
-        "hero-inner section-inner",
-        topDivider && "has-top-divider",
-        bottomDivider && "has-bottom-divider"
-    );
+  const innerClasses = classNames(
+    "hero-inner section-inner",
+    topDivider && "has-top-divider",
+    bottomDivider && "has-bottom-divider"
+  );
 
-    return (
-        <section {...props} className={outerClasses}>
-            <div className="container-sm">
-                <div className={innerClasses}>
-                    <div className="hero-content">
-                        <h1
-                            className="mt-0 mb-16 reveal-from-bottom"
-                            data-reveal-delay="200"
-                        >
-                            <span className="text-color-secondary">
-                                Hamster
-                            </span>{" "}
-                            Health
-                        </h1>
-                        <div className="container-xs">
-                            <p
-                                className="m-0 mb-32 reveal-from-bottom"
-                                data-reveal-delay="400"
-                            >
-                                Task organiser focused on self improvement
-                                during quarantine
-                            </p>
-                            <div
-                                className="reveal-from-bottom"
-                                data-reveal-delay="600"
-                            >
-                                <ButtonGroup>
-                                    <Link to="/register">
-                                        <Button
-                                            color="primary"
-                                            wideMobile
-                                            className="mt-4 mb-0"
-                                        >
-                                            Get started
-                                        </Button>
-                                    </Link>
-                                </ButtonGroup>
-                            </div>
-                        </div>
-                    </div>
-                    <div
-                        className="hero-figure reveal-from-bottom illustration-element-01"
-                        data-reveal-value="20px"
-                        data-reveal-delay="800"
-                    >
-                        <a
-                            data-video="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-                            href="#0"
-                            aria-controls="video-modal"
-                            onClick={openModal}
-                        >
-                            <Image
-                                className="has-shadow"
-                                src={require("./../../assets/images/video-placeholder.jpg")}
-                                alt="Hero"
-                                width={896}
-                                height={504}
-                            />
-                        </a>
-                    </div>
-                    <Modal
-                        id="video-modal"
-                        show={videoModalActive}
-                        handleClose={closeModal}
-                        video="https://www.youtube.com/embed/dQw4w9WgXcQ"
-                        videoTag="iframe"
-                    />
-                </div>
+  return (
+    <section {...props} className={outerClasses}>
+      <div className="container-sm">
+        <div className={innerClasses}>
+          <div className="hero-content">
+            <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
+              <span className="text-color-secondary">Hamster</span> Health
+            </h1>
+            <div className="container-xs">
+              <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
+                An organisational task tracking tool designed to help you achieve your goals
+              </p>
+              <div className="reveal-from-bottom" data-reveal-delay="600">
+                <ButtonGroup>
+                  <Link to="/register">
+                    <Button color="primary" wideMobile className="mt-4 mb-0">
+                      Get started
+                    </Button>
+                  </Link>
+                </ButtonGroup>
+              </div>
             </div>
-        </section>
-    );
+          </div>
+          <div
+            className="hero-figure reveal-from-bottom illustration-element-01"
+            data-reveal-value="20px"
+            data-reveal-delay="800"
+          >
+            <a
+              data-video="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+              href="#0"
+              aria-controls="video-modal"
+              onClick={openModal}
+            >
+              <Image
+                className="has-shadow"
+                src={require("./../../assets/images/video-placeholder.jpg")}
+                alt="Hero"
+                width={896}
+                height={504}
+              />
+            </a>
+          </div>
+          <Modal
+            id="video-modal"
+            show={videoModalActive}
+            handleClose={closeModal}
+            video="https://www.youtube.com/embed/dQw4w9WgXcQ"
+            videoTag="iframe"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 Hero.propTypes = propTypes;
